8a1a24819c936b49650c60a22cf628f3a8067940
sonyahon <volkov030@gmail.com>
2024-02-17 14:37:58 +0400

    Fix camera lookat

--- engine/src/camera.ts
+++ engine/src/camera.ts
@@ -1,103 +1,104 @@
-import { logger } from './logger'
-import { Matrix4f, Vector3 } from './math'
-import { degreesToRadians, radiansToDegrees } from './math/functions'
+import {degreesToRadians, Matrix4f, radiansToDegrees, Vector3} from './math'
+import {logger} from "./logger";
 
 export class Camera3d {
-  private _matrixCache: Matrix4f | null = null
+    private _matrixCache: Matrix4f | null = null
 
-  constructor(
-    public position: Vector3 = Vector3.zero(),
-    public rotation: Vector3 = Vector3.zero()
-  ) {}
-
-  viewMatrix() {
-    if (this._matrixCache) {
-      return this._matrixCache
+    constructor(
+        public position: Vector3 = Vector3.zero(),
+        public rotation: Vector3 = Vector3.zero()
+    ) {
     }
 
-    this._matrixCache = Matrix4f.identity()
-      .rotate(degreesToRadians(this.rotation.x), Vector3.right())
-      .rotate(degreesToRadians(this.rotation.y), Vector3.up())
-      .translate(new Vector3(-this.position.x, -this.position.y, -this.position.z))
+    viewMatrix() {
+        if (this._matrixCache) {
+            return this._matrixCache
+        }
 
-    return this._matrixCache
-  }
+        this._matrixCache = Matrix4f.identity()
+            .rotate(degreesToRadians(this.rotation.x), Vector3.right())
+            .rotate(degreesToRadians(this.rotation.y), Vector3.up())
+            .translate(new Vector3(-this.position.x, -this.position.y, -this.position.z))
 
-  translate(v: Vector3): this {
-    this.position.add(v)
-    this._matrixCache = null
-    return this
-  }
+        return this._matrixCache
+    }
 
-  setPosition(v: Vector3): this {
-    this.position = v
-    this._matrixCache = null
-    return this
-  }
+    translate(v: Vector3): this {
+        this.position.add(v)
+        this._matrixCache = null
+        return this
+    }
 
-  translateX(x: number): this {
-    this.position.x += x
-    this._matrixCache = null
-    return this
-  }
+    setPosition(v: Vector3): this {
+        this.position = v
+        this._matrixCache = null
+        return this
+    }
 
-  translateY(y: number): this {
-    this.position.y += y
-    this._matrixCache = null
-    return this
-  }
+    translateX(x: number): this {
+        this.position.x += x
+        this._matrixCache = null
+        return this
+    }
 
-  translateZ(z: number): this {
-    this.position.z += z
-    this._matrixCache = null
-    return this
-  }
+    translateY(y: number): this {
+        this.position.y += y
+        this._matrixCache = null
+        return this
+    }
 
-  rotate(v: Vector3): this {
-    this.rotation.add(v)
-    this._matrixCache = null
-    return this
-  }
+    translateZ(z: number): this {
+        this.position.z += z
+        this._matrixCache = null
+        return this
+    }
 
-  setRotation(v: Vector3): this {
-    this.rotation = v
-    this._matrixCache = null
-    return this
-  }
+    rotate(v: Vector3): this {
+        this.rotation.add(v)
+        this._matrixCache = null
+        return this
+    }
 
-  rotateX(x: number): this {
-    this.rotation.x += x
-    this._matrixCache = null
-    return this
-  }
+    setRotation(v: Vector3): this {
+        this.rotation = v
+        this._matrixCache = null
+        return this
+    }
 
-  rotateY(y: number): this {
-    this.rotation.y += y
-    this._matrixCache = null
-    return this
-  }
+    rotateX(x: number): this {
+        this.rotation.x += x
+        this._matrixCache = null
+        return this
+    }
 
-  rotateZ(z: number): this {
-    this.rotation.z += z
-    this._matrixCache = null
-    return this
-  }
+    rotateY(y: number): this {
+        this.rotation.y += y
+        this._matrixCache = null
+        return this
+    }
 
-  lookAt(target: Vector3, up: Vector3 = Vector3.up()) {
-    const direction = Vector3.subtract(this.position, target).normalize()
-    const right = Vector3.cross(up, direction).normalize()
-    up = Vector3.cross(direction, right).normalize()
+    rotateZ(z: number): this {
+        this.rotation.z += z
+        this._matrixCache = null
+        return this
+    }
 
-    const rotationX = Math.asin(direction.y)
-    const rotationY = Math.atan2(direction.x, direction.z)
-    const rotationZ = Math.atan2(right.y, up.y)
+    lookAt(target: Vector3, up: Vector3 = Vector3.up()) {
+        const direction = Vector3.subtract(this.position, target).normalize()
+        const right = Vector3.cross(up, direction).normalize()
+        up = Vector3.cross(direction, right).normalize()
 
-    this.rotation = new Vector3(
-      radiansToDegrees(rotationX),
-      radiansToDegrees(rotationY),
-      radiansToDegrees(rotationZ)
-    )
-    this._matrixCache = null
-    return this
-  }
+        const rotationX = Math.asin(direction.y)
+        const rotationY = -Math.atan2(direction.x, direction.z)
+        const rotationZ = Math.atan2(right.y, up.y)
+
+        this.rotation = new Vector3(
+            radiansToDegrees(rotationX),
+            radiansToDegrees(rotationY),
+            radiansToDegrees(rotationZ)
+        )
+        logger.debug('Camera3d.lookAt', this.rotation)
+        this._matrixCache = null
+        return this
+    }
 }
--- sandbox/src/index.ts
+++ sandbox/src/index.ts
@@ -1,44 +1,57 @@
-import Joy, {
-  GeometryData,
-  Color,
-  Transform,
-  Vector3,
-  logger,
-  GameObject,
-  Material,
-  Mesh,
-} from '@joy/engine'
+import Joy, {Color, GameObject, GeometryData, logger, Material, Mesh, Vector3,} from '@joy/engine'
+
+const colors = [
+    "#0055ff",
+    "#ff0055",
+    "#55ff00",
+];
 
 async function main() {
-  let cubes: GameObject[] = new Array(3)
+    let cubes: GameObject[] = new Array(3)
 
-  Joy.onInitialize(async () => {
-    Joy.setClearColor(Color.fromHex('#efefef'))
+    Joy.onInitialize(async () => {
+        Joy.setClearColor(Color.fromHex('#e2e2e2'))
 
-    for (let i = 0; i < 3; i++) {
-      cubes[i] = new GameObject()
-      cubes[i].addComponent(
-        new Material(Joy.shader.standard, {
-          solidColor: Color.fromHex('#0055ff'),
+        for (let i = 0; i < 3; i++) {
+            cubes[i] = new GameObject()
+            cubes[i].addComponent(
+                new Material(Joy.shader.standard, {
+                    solidColor: Color.fromHex(colors[i]),
+                })
+            )
+            cubes[i].addComponent(new Mesh(GeometryData.cube()))
+            cubes[i].transform.setPosition(new Vector3(0, -4 + i * 4, 0))
+        }
+
+        Joy.camera3d.setPosition(new Vector3(20, 20, 20)).lookAt(Vector3.zero())
+    })
+
+    Joy.onUpdate(() => {
+        cubes.forEach((cube) => {
+            cube.transform
+                .rotate(Vector3.unit(Joy.time.deltaTime * 20))
+                // .setPosition(
+                //     new Vector3(
+                //         Math.sin(
+                //             Joy.time.elapsedTotal + cube.transform.position.x
+                //         ),
+                //         cube.transform.position.y,
+                //         Math.cos(
+                //             Joy.time.elapsedTotal + cube.transform.position.z
+                //         )
+                //     )
+                // )
         })
-      )
-      cubes[i].addComponent(new Mesh(GeometryData.cube()))
-      cubes[i].transform.setPosition(new Vector3(0, 0, i * 4))
-    }
+    })
 
-    Joy.camera3d.setPosition(new Vector3(0, 20, 20)).lookAt(Vector3.zero())
+    Joy.onRender(() => {
+        const renderer = Joy.$internal$__getRenderer()
+        cubes.forEach((cube) => renderer.renderGameObject(cube))
+    })
 
-  })
-
-  Joy.onUpdate(() => {
-  })
-
-  Joy.onRender(() => {
-    const renderer = Joy.$internal$__getRenderer()
-    cubes.forEach((cube) => renderer.renderGameObject(cube))
-  })
-
-  await Joy.run()
+    await Joy.run()
 }
 
-main()
+main().catch(error => {
+    logger.panic('Uncaught exception', error)
+})
